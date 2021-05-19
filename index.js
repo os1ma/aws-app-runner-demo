c6e78fbd3b79ba8384d889d60e5ed0702051f724
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-19 13:47:21 +0900

    SIGTERM のハンドラを追加

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+process.on('SIGTERM', () => {
+  console.log('Received SIGTERM...');
+});
