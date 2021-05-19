e20879afef1d2e27e21a3e2a6dadbee56ca9ce6a
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2021-05-19 13:29:44 +0900

    レスポンスを遅らせるためのパラメータを追加

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const uuid = require('uuid');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  const requestId = uuid.v4();
+  console.log(`[${requestId}] [request] path = ${req.path}`);
+
+  const delaySec = req.query.delay || 0;
+
+  setTimeout(() => {
+    console.log(`[${requestId}] [response]`);
+    res.send('Hello World!');
+  }, delaySec * 1000);
 });
 
 app.listen(port, () => {
